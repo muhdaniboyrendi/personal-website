a763faa7160276a3e45545555152a36e15c7ed9e
Muhdani Boyrendi Erlan Azhari <muhdaniboyrendi@gmail.com>
2024-11-04 07:40:00 +0700

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/muhdaniboyrendi.github.io/',
+  base: '/personal-website/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
